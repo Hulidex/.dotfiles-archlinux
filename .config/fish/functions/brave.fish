b9ed3097fdb204aa035f3e804b68eaf9b16422ca
Jose Luis Izquierdo <Hulidex@users.noreply.github.com>
2022-01-22 20:04:36 +0100

    Add tray
    Add tray module to swaybar

--- .config/fish/functions/brave.fish
+++
@@ -1,3 +0,0 @@
-function brave --description 'alias brave brave --enable-features=UseOzonePlatform --ozone-platform=wayland'
- command brave --enable-features=UseOzonePlatform --ozone-platform=wayland $argv; 
-end
